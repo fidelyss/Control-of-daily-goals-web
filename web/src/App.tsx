3cb4a1e2d5e4c2b1871f4691ff8153effbf57968
fidelyss <alissonfidelis147@gmail.com>
2025-07-05 20:19:08 -0300

    web 0.1

---
+++ web/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+import logo from '/task-runners-svgrepo-com.svg'
+function App() {
+  return (
+      <div className='h-screen flex flex-col items-center justify-center gap-8'>
+        <img className='"w-[100px] h-[100px]' src={logo} alt="" />
+      </div>
+  )
+}
+
+export default App
