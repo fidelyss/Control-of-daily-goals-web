c495c7002f52b7edacdd387e71a1a6462a3dc21b
fidelyss <alissonfidelis147@gmail.com>
2025-07-07 20:34:41 -0300

    web 0.2

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,21 @@
 import './App.css'
-import logo from '/task-runners-svgrepo-com.svg'
+import logo from '/METAAPP.svg'
+import startLogo from '/start-svgrepo-com.svg'
+import { Plus } from 'lucide-react'
+import { Button } from './Components/ui/button'
 function App() {
   return (
-      <div className='h-screen flex flex-col items-center justify-center gap-8'>
-        <img className='"w-[100px] h-[100px]' src={logo} alt="" />
-      </div>
+    <div className='h-screen flex flex-col items-center justify-center gap-8'>
+      <img className='"w-[100px] h-[100px]' src={logo} alt="META APP" />
+      <img className='"w-[150px] h-[400px]' src={startLogo} alt="Start" />
+      <p className='text-zinc-300 leading-relaxed max-w-80 text-center'>
+        Você ainda Não cadastrou nenhuma meta, que tal cadastrar uma agora mesmo ?
+      </p>
+      <Button>
+        <Plus className='size-4' />
+        Cadastrar meta
+      </Button >
+    </div>
   )
 }
 
